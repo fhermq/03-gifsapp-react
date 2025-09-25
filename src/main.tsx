ad301d8f16b41cadaf4d41e2a1fc9dca2e18ea02
Fernando Mirasol <fhermq@gmail.com>
2025-09-25 17:05:36 -0600

    Se prepara version para build

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { GifsApp } from './GifsApp'
-import { MyCounterApp } from './counter/components/MyCounterApp'
 
 
 createRoot(document.getElementById('root')!).render(
