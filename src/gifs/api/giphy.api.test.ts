b263340204ddb5eabb430d4373f8d8d9c8f14914
Fernando Mirasol <fhermq@gmail.com>
2025-09-29 17:11:24 -0600

    Se agregan Test de AXIOS

---
+++ src/gifs/api/giphy.api.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, test } from "vitest";
+import { giphyApi } from "./giphy.api";
+
+describe('giphy.api', () => {
+
+    test('should be configured correctly.', () => {
+        // console.log(giphyApi.defaults);
+        const params = giphyApi.defaults.params;
+
+        //To Be para primitivos
+        expect(giphyApi.defaults.baseURL).toBe('https://api.giphy.com/v1/gifs');
+
+        expect(params).toStrictEqual({
+            lang: 'es',
+            api_key: import.meta.env.VITE_GIPHY_API_KEY,
+        });
+
+
+    })
+
+});
